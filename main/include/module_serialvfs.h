d418193ef8013a17dd51d1c22b212473b5d92f4b
David Ashley <dashxdr@gmail.com>
2018-06-12 18:37:02 -0500

    Implemented a serial port virtual filesystem solution. This allows much faster development than the wifi network vfs solution, in my case it now takes about 1.8 seconds from reboot to load app/main.js served from the serial port at 115,200 baud vs around 15 seconds to bootwifi and connect to my access point and setup NetVFS...
    I tried running the baud up to 921,600 but it's not reliable. I think the
    uart RX buffer needs to be bigger.

---
+++ main/include/module_serialvfs.h
@@ -0,0 +1,14 @@
+/*
+ * module_serialvfs.h
+ *
+ *  Created on: Jun 12, 2018
+ *      Author: dashxdr
+ */
+
+#ifndef MAIN_INCLUDE_MODULE_SERIALVFS_H_
+#define MAIN_INCLUDE_MODULE_SERIALVFS_H_
+#include <duktape.h>
+
+duk_ret_t ModuleSerialVFS(duk_context *ctx);
+
+#endif /* MAIN_INCLUDE_MODULE_SERIALVFS_H_ */
